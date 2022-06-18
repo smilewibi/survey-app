ffcd57ba891a46e6afa16450ba4e217e118a2bcd
ismail <wibidjokam@gmail.com>
2022-06-18 15:33:21 +0700

    Create Survey

--- vue/src/components/PageComponent.vue
+++ vue/src/components/PageComponent.vue
@@ -1,14 +1,17 @@
 <template>
   <header class="bg-white shadow">
     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold text-gray-900">{{ title }}</h1>
+      <h1 v-if="title" class="text-3xl font-bold text-gray-900">
+        {{ title }}
+      </h1>
+      <slot name="header"></slot>
     </div>
   </header>
   <main>
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
       <!-- Replace with your content -->
-      <div class="px-4 py-6 sm:px-0">
-          <slot></slot>
+      <div class="px-4 sm:px-0">
+        <slot></slot>
       </div>
       <!-- /End replace -->
     </div>
@@ -17,10 +20,8 @@
 
 <script setup>
 const props = defineProps({
-    title: String,
+  title: String,
 });
 </script>
 
-<style>
-
-</style>
+<style></style>
